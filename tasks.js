ddb15ff405d8231c5ad6f8d353701f5c97285b88
Thomas Morales <thomasmorales@gmail.com>
2019-03-25 01:52:35 +0400

    test

--- tasks.js
+++ tasks.js
@@ -23,6 +23,7 @@ for (var i = spans.length - 1; i >= 0; i--) {
    let filename = event.target.src.replace("https://thomasmorales.github.io/icons/", "");
   //let fileName= event.target.src.replace("file:///D:/Desktop/pl_projet1_html/icons/flower1.png",'flower1');
     document.getElementById("test").innerHTML = event.target.src;
+    document.getElementById("test1").innerHTML = fileName;
   if (event.target.src) {
     if (fileName=="flower1"){
       event.target.src = "icons/flower2.png";
